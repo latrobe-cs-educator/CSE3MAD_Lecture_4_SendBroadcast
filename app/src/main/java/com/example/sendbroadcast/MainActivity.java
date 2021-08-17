3b0e8f15d9b2bb1d2ee9826aa17ccd709b2c701d
Marita Fitzgerald <maritafitzgerald@gmail.com>
2021-08-18 06:41:13 +1000

    Update button

--- app/src/main/java/com/example/sendbroadcast/MainActivity.java
+++ app/src/main/java/com/example/sendbroadcast/MainActivity.java
@@ -7,22 +7,19 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     BroadcastReceiver rx;
+    Button sendBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        configureReceiver();
-    }
-
-    //Register Broadcast Receiver
-    private void configureReceiver()
-    {
+        //Configure receiver
         IntentFilter filter = new IntentFilter();
         //listen for app action
         filter.addAction("com.example.sendbroadcast");
@@ -30,13 +27,19 @@ public class MainActivity extends AppCompatActivity {
         filter.addAction("android.intent.action.ACTION_POWER_DISCONNECTED");
         rx = new MyReceiver();
         registerReceiver(rx, filter);
-    }
 
-    public void broadcastIntent(View view) {
-        Intent intent = new Intent();
-        intent.setAction("com.example.sendbroadcast");
-        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
-        sendBroadcast(intent);
+        //Set up button
+        sendBtn = (Button)findViewById(R.id.button);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction("com.example.sendbroadcast");
+                intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+                sendBroadcast(intent);
+            }
+        });
+
     }
 
     //Unregister the broadcast receiver when it is no longer needed:
